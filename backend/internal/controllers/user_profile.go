10166bf93fdf8ac6694cc6d8664cfbf1125230a7
OumarLAM <oumarlam154@gmail.com>
2024-03-06 06:05:22 +0000

    Handle the UpdateProfilePrivacy handler

--- backend/internal/controllers/user_profile.go
+++ backend/internal/controllers/user_profile.go
@@ -2,11 +2,9 @@ package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/OumarLAM/SocialFace/internal/models"
-	"github.com/google/uuid"
 )
 
 // ProfileInfoHandler retrieves user profile information based on the user ID.
@@ -15,7 +13,7 @@ func ProfileInfoHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-	
+
 	// Retrieve userID from the context
 	userID, ok := r.Context().Value("userID").(int)
 	if !ok {
@@ -37,6 +35,11 @@ func ProfileInfoHandler(w http.ResponseWriter, r *http.Request) {
 
 // UpdateProfilePrivacyHandler updates the profile privacy settings for the authenticated user
 func UpdateProfilePrivacyHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+        return
+    }
+
 	// Parse request body to get new privacy setting
 	var privacySetting struct {
 		PublicProfile bool `json:"public_profile"`
@@ -47,11 +50,11 @@ func UpdateProfilePrivacyHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Extract userID from session token stored in the cookie
-	userID, err := DecodeSessionToken(r)
-	if err != nil {
-		http.Error(w, "Failed to decode session token", http.StatusInternalServerError)
-		return
+	// Retrieve userID from the context
+	userID, ok := r.Context().Value("userID").(int)
+	if !ok {
+		http.Error(w, "User ID not found in context", http.StatusInternalServerError)
+        return
 	}
 
 	// Update user profile privacy settings in the database
@@ -64,28 +67,4 @@ func UpdateProfilePrivacyHandler(w http.ResponseWriter, r *http.Request) {
 	// Respond with updated user profile privacy settings
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"message": "Profile privacy settings updated successfully"})
-}
-
-// DecodeSessionToken decodes the session token to extract the userID.
-func DecodeSessionToken(r *http.Request) (int, error) {
-	// Extract the session token from cookie
-	cookie, err := r.Cookie("session_token")
-	if err != nil {
-		return 0, err
-	}
-
-	fmt.Println(cookie)
-
-	// Parse session token as UUID
-	sessionToken, err := uuid.Parse(cookie.Value)
-	fmt.Println(sessionToken)
-	if err != nil {
-		return 0, err
-	}
-
-	// Extract the userID from the session token
-	userID := int(sessionToken.ID())
-	fmt.Println(userID)
-
-	return userID, nil
 }
