802a81a66aaa9d5d1154de376be79ddf22170c7f
OumarLAM <oumarlam154@gmail.com>
2024-04-01 21:56:58 +0000

    Handle routing logic with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import { Home, Login, Register, Profile } from "./pages";
+
+function Layout() {
+  const user = null;
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
+}
+
 function App() {
   return (
     <div className="w-full min-h-[100vh]">
-      <h1 className="text-4xl font-bold text-center text-blue">Welcome to Socialface</h1>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/:id?" element={<Profile />} />
+        </Route>
+
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Login from "./Login";
+import Register from "./Register";
+import Home from "./Home";
+import Profile from "./Profile";
+
+export { Login, Register, Home, Profile };
